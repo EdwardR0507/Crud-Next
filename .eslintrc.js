58e3fec3e32b4b0887eba173cdb557d3c550463e
EdwardR0507 <ramosedward21@gmail.com>
2022-03-18 20:25:51 -0500

    feat: Login

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended",
-    "next",
-    "next/core-web-vitals",
-  ],
+  extends: ["eslint:recommended", "plugin:jsx-a11y/recommended", "plugin:prettier/recommended", "next", "next/core-web-vitals"],
   rules: {
     semi: [
       "error",
